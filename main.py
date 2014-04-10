af524d15c10d4f3c98826bcf835d942a1acbdc4b
Dan Moseley <danomoseley@gmail.com>
2014-04-10 12:03:52 -0400

    Pagination

--- main.py
+++ main.py
@@ -23,9 +23,9 @@ def findCategory(searchTerm=None, categoryId=None):
 	if searchTerm is None:
 		searchTerm = request.form['searchTerm']
 	if categoryId is not None:
-		api.execute('findCompletedItems', {'keywords': searchTerm, 'outputSelector': 'CategoryHistogram', 'categoryId':categoryId})
+		api.execute('findCompletedItems', {'sortOrder': 'EndTimeSoonest', 'keywords': searchTerm, 'outputSelector': 'CategoryHistogram', 'categoryId':categoryId})
 	else:
-		api.execute('findCompletedItems', {'keywords': searchTerm, 'outputSelector': 'CategoryHistogram'})
+		api.execute('findCompletedItems', {'sortOrder': 'EndTimeSoonest','keywords': searchTerm, 'outputSelector': 'CategoryHistogram'})
 	response = api.response_dict()
 
 	if type(response['categoryHistogramContainer']['categoryHistogram']) is not list:
@@ -46,8 +46,9 @@ def findCategory(searchTerm=None, categoryId=None):
 
 @app.route("/statistics/<searchTerm>/<categoryId>")
 def statistics(searchTerm=None, categoryId=None):
-	api.execute('findCompletedItems', {'keywords': searchTerm, 'categoryId': str(categoryId), 'itemFilter': 'SoldItemsOnly'})
 	api2.execute('GetCategoryInfo', {'CategoryID': str(categoryId)})
+	
+	
 
 	categoryName = api2.response_dict()['CategoryArray']['Category']['CategoryName']['value']
 
@@ -56,17 +57,31 @@ def statistics(searchTerm=None, categoryId=None):
 
 	soldPrices = []
 
-	for item in api.response_dict()['searchResult']['item']:
-		if item['sellingStatus']['sellingState']['value'] == 'EndedWithoutSales':
-			unsold += 1
-		else:
-			sold += 1
-			soldPrices.append(float(item['sellingStatus']['currentPrice']['value']))
+	earliestSoldDate = None
+	latestSoldDate = None
+	currentPage = 0
+	totalPages = 1
+	while currentPage < 10 and currentPage < totalPages:
+		api.execute('findCompletedItems', {'paginationInput.pageNumber': currentPage, 'sortOrder': 'EndTimeSoonest','keywords': searchTerm, 'categoryId': str(categoryId), 'itemFilter': 'SoldItemsOnly'})
+		response = api.response_dict()
+		totalPages = int(response['paginationOutput']['totalPages']['value'])
+
+		for item in response['searchResult']['item']:
+			if item['sellingStatus']['sellingState']['value'] == 'EndedWithoutSales':
+				unsold += 1
+			else:
+				soldPrices.append(float(item['sellingStatus']['currentPrice']['value']))
+				if sold == 0:
+					latestSoldDate = item['listingInfo']['endTime']['value']
+				else:
+					earliestSoldDate = item['listingInfo']['endTime']['value']
+				sold += 1
+		currentPage += 1
 
 	meanPrice = round(numpy.mean(soldPrices), 2)
 	priceStdDev = round(numpy.std(soldPrices), 2)
 	greatDeal = round(meanPrice - priceStdDev, 2)
-	return render_template('statistics.html', categoryName=categoryName, searchTerm=searchTerm, sold=sold, unsold=unsold, meanPrice=meanPrice, priceStdDev=priceStdDev, greatDeal=greatDeal)
+	return render_template('statistics.html', earliestSoldDate=earliestSoldDate, latestSoldDate=latestSoldDate, categoryName=categoryName, searchTerm=searchTerm, sold=sold, unsold=unsold, meanPrice=meanPrice, priceStdDev=priceStdDev, greatDeal=greatDeal)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
